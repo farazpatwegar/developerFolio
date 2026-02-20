a9d510fb8e0af3f6fbf26bf27f55f385592ec278
farazpatwegar <farazpatwegar@gmail.com>
2026-02-20 20:12:30 +0530

    Number change

--- src/portfolio.js
+++ src/portfolio.js
@@ -386,7 +386,7 @@ const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+92-0000000000",
+  number: "+91-9834401497",
   email_address: "farazpatwegar@gmail.com"
 };
 
