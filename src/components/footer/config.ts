b7a61c3ff9374579fbecef5de8e707b38440c571
farazpatwegar <farazpatwegar@gmail.com>
2026-03-10 22:35:21 +0530

    Initial commit

---
+++ src/components/footer/config.ts
@@ -0,0 +1,12 @@
+const footer: { title: string; href: string }[] = [
+  {
+    title: "Blog",
+    href: "https://farazpatwegar.hashnode.dev/",
+  },
+  {
+    title: "Newsletter",
+    href: "https://farazpatwegar.hashnode.dev/",
+  },
+];
+
+export { footer };
---
+++ src/data/config.ts
@@ -0,0 +1,48 @@
+const config = {
+  title: "Faraz Patwegar | Software Developer",
+  description: {
+    long: "Explore the portfolio of Faraz , a Computer Science Engineer with 1.5 year experience in Devlopment, DevOps & Database management with hands-on experience in deploying & automating workflows, & collaborating with foreign clients. Strong analytical skills, effective communication, and a team-oriented approach to driving process improvements and delivering results.",
+    short:
+      "Discover the portfolio of Faraz , a AI enthusiast solving real life problems in DevOps and Development.",
+  },
+  keywords: [
+    "Faraz ",
+    "Faraz Patwegar",
+    "farazpatwegar",
+    "farazpatwegar.com",
+    "Faraz Patwegar portfolio",
+    "portfolio",
+    "DevOps",
+    "Devlopment",
+    "Database",
+    "AI Docker file optimizer",
+    "Finance me",
+    "creative technologist",
+    "web development",
+    "interactive websites",
+    // "GSAP",
+    // "React",
+    // "Next.js",
+    // "Spline",
+    // "Framer Motion",
+    "AWS",
+    "DevSecOps",
+    "AIOps",
+    "Azure",
+  ],
+  author: "Faraz Patwegar",
+  email: "farazpatwegar@gmail.com",
+  site: "https://farazpatwegar.com",
+
+  get ogImg() {
+    return this.site + "/assets/seo/og-image.png";
+  },
+  social: {
+    twitter: "https://x.com/_Abhi_Speaks/",
+    linkedin: "https://www.linkedin.com/in/faraz-patwegar/",
+    instagram: "https://www.instagram.com/_.celestial_abhi._/",
+    facebook: "https://www.facebook.com/profile.php?id=100010029821759",
+    github: "https://github.com/farazpatwegar/",
+  },
+};
+export { config };
